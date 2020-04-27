ef91bfa828d49e73b1f5c9260f3d6d35afb45ac7
Ravi <ravishankarsingh1996@gmail.com>
2020-04-27 21:22:10 +0530

    Day 2

--- myFirst.js
+++ myFirst.js
@@ -4,6 +4,7 @@ var date = require('./myFirstModule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('The date and time are currently:' + date.myDateTime());
-  res.end('Hello World!');
+  res.write(req.url);
+  // res.write('The date and time are currently:' + date.myDateTime());
+  res.end();
 }).listen(8080);
